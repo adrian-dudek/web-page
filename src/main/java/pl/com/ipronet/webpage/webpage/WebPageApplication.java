70ea4728e3f7c3de4c78691c5969a81a404210d5
adrian-dudek <contact@jmodule.org>
2017-11-01 00:16:12 +0100

    Initial commit

---
+++ src/main/java/pl/com/ipronet/webpage/webpage/WebPageApplication.java
@@ -0,0 +1,12 @@
+package pl.com.ipronet.webpage.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPageApplication.class, args);
+	}
+}
